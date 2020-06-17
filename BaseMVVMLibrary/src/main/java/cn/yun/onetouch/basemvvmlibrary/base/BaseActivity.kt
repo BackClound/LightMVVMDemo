61ccb622b5f9a4cfe389187e7477891883624cb9
Yun <1668430002@qq.com>
2020-06-17 19:58:57 +0800

    add dialog util

--- BaseMVVMLibrary/src/main/java/cn/yun/onetouch/basemvvmlibrary/base/BaseActivity.kt
+++ BaseMVVMLibrary/src/main/java/cn/yun/onetouch/basemvvmlibrary/base/BaseActivity.kt
@@ -1,4 +1,32 @@
 package cn.yun.onetouch.basemvvmlibrary.base
 
-class BaseActivity {
+import androidx.databinding.ViewDataBinding
+import androidx.viewbinding.ViewBinding
+import com.afollestad.materialdialogs.MaterialDialog
+import com.trello.rxlifecycle2.components.support.RxAppCompatActivity
+
+/**
+ * 一个拥有DataBinding框架的基Activity
+ * 这里根据项目业务可以换成你自己熟悉的BaseActivity, 但是需要继承RxAppCompatActivity,方便LifecycleProvider管理生命周期
+ */
+
+abstract class BaseActivity<V : ViewDataBinding, VM : BaseViewModel<*>> : RxAppCompatActivity(),
+    IBaseView {
+
+    private var binding: V? = null
+    private var viewModel: VM? = null
+    private var viewModelId: Int? = null
+//    private val dialog : MaterialDialog
+
+    override fun initParams() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initData() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initViewObservable() {
+        TODO("Not yet implemented")
+    }
 }
--- BaseMVVMLibrary/src/main/java/cn/yun/onetouch/basemvvmlibrary/utils/materialDialogUtils.kt
+++ BaseMVVMLibrary/src/main/java/cn/yun/onetouch/basemvvmlibrary/utils/materialDialogUtils.kt
@@ -1,4 +1,31 @@
 package cn.yun.onetouch.basemvvmlibrary.utils
 
-class materialDialogUtils {
+import android.content.Context
+import android.content.DialogInterface
+import android.graphics.Color
+import cn.yun.onetouch.basemvvmlibrary.R
+import com.afollestad.materialdialogs.DialogAction
+import com.afollestad.materialdialogs.GravityEnum
+import com.afollestad.materialdialogs.MaterialDialog
+import com.afollestad.materialdialogs.Theme
+
+object materialDialogUtils {
+
+    fun showThemed(context: Context, title: String, content: String) {
+        MaterialDialog.Builder(context).title(title).content(content).positiveText("agree")
+            .negativeText("disagree").positiveColorRes(R.color.white)
+            .negativeColorRes(R.color.white).titleGravity(GravityEnum.CENTER)
+            .titleColorRes(R.color.white).contentColorRes(R.color.white)
+            .backgroundColorRes(R.color.material_blue_grey_800).dividerColorRes(R.color.white)
+            .btnSelector(R.drawable.md_selector, DialogAction.POSITIVE)
+            .positiveColor(Color.WHITE)
+            .theme(Theme.DARK).autoDismiss(true)
+            .showListener { //Dialog show
+            }.cancelListener {
+                //dialog dis
+            }.dismissListener {
+
+            }.show()
+    }
+}
 }
